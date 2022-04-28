f7bd5363de1c5572bdc512a789eeab0e948687c0
Czékus Máté <mate@picloud.hu>
2022-04-28 17:19:12 +0200

    Add tailwind
    Signed-off-by: Czékus Máté <mate@picloud.hu>

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,5 +2,7 @@ import {defineNuxtConfig} from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false // static SPA
+    ssr: false, // static SPA
+    modules: ['@nuxtjs/tailwindcss'], // extensions,
+    telemetry: false, // disable telemetry
 })
