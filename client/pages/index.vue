01ff1c778f7a523dc1d399877a83b00ee2a12f77
Adam Varga <github@lightless.nl>
2022-04-28 16:39:05 +0200

    nuxt3 frontend init
    Signed-off-by: Adam Varga <github@lightless.nl>

---
+++ client/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Index Page</h1>
+  <NuxtLink to="/test">To Test Page</NuxtLink>
+</template>
---
+++ client/pages/test-[text].vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Test Page</h1>
+  <p>{{$route.params.text}}</p>
+  <NuxtLink to="/">To Index Page</NuxtLink>
+</template>
